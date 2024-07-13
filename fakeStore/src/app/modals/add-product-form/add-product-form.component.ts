7e885ec70c6a0cd5642539b8ca3df9c445539f27
Mikołaj <mikol.cicho@gmail.com>
2024-07-13 16:41:11 +0200

    category in form

--- fakeStore/src/app/modals/add-product-form/add-product-form.component.ts
+++ fakeStore/src/app/modals/add-product-form/add-product-form.component.ts
@@ -1,7 +1,9 @@
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { ApiService } from '../../services/api.service';
 import { Component } from '@angular/core';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-add-product-form',
@@ -9,14 +11,16 @@ import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AddProductFormComponent {
   form: FormGroup;
+  categories: string[] = []
 
-  constructor(public activeModal: NgbActiveModal) {
+  constructor(public activeModal: NgbActiveModal, private apiService: ApiService) {
     this.form = new FormGroup({
       title: new FormControl(''),
       category: new FormControl(''),
       price: new FormControl(''),
       description: new FormControl('')
     });
+    this.loadCategories();
   }
 
   closeModal() {
@@ -29,5 +33,13 @@ export class AddProductFormComponent {
     } else {
       console.log('form is not valid')
     }
+  }
+
+  loadCategories() {
+    this.apiService.fetchAllCategories().pipe(
+      tap(categories => {
+        this.categories = categories;
+      })
+    ).subscribe();
   }
 }
